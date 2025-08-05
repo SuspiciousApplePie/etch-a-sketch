e9a07a781c9c3fff7788618cc06f536867681e56
Shishi <andreiusita6@gmail.com>
2025-08-05 21:46:37 +0800

    feat: Add range validation in initGrid()

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ function initGrid (size) {
         size = prompt('Set the grid (1-100):');
         let convertSize = +size;
         if (!Number.isInteger(convertSize)) {
-            alert('Please enter a number')
+            alert('Please enter a number');
+        } else if (convertSize < 1 || convertSize > 100) {
+            alert('Your input is lower than 1 or higher than 100');
         };
     })
 }
