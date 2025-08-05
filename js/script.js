ab68f747ca3dba1a8064bab1d2139eab9a0cf56e
Shishi <andreiusita6@gmail.com>
2025-08-05 21:41:32 +0800

    feat: Add number validation in initGrid()

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ function initGrid (size) {
     const adjustBtn = document.querySelector('.adjust-btn');
     adjustBtn.addEventListener('click', () => {
         size = prompt('Set the grid (1-100):');
+        let convertSize = +size;
+        if (!Number.isInteger(convertSize)) {
+            alert('Please enter a number')
+        };
     })
 }
 
