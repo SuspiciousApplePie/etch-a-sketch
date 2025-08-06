33a9d2eb80aa236654aa7ad33984d2ae4e85aeb6
Shishi <andreiusita6@gmail.com>
2025-08-06 08:16:55 +0800

    feat: Add empty string validation in initGrid()

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@ function initGrid (size) {
     const adjustBtn = document.querySelector('.adjust-btn');
     adjustBtn.addEventListener('click', () => {
         size = prompt('Set the grid (1-100):');
-        let convertSize = +size;
-        if (!Number.isInteger(convertSize)) {
+        if (size === null) {
+            return;
+        } else if (size.length === 0) {
+            alert('Empty input');
+        } else if (!Number.isInteger(+size)) {
             alert('Please enter a number');
-        } else if (convertSize < 1 || convertSize > 100) {
+        } else if (+size < 1 || +size > 100) {
             alert('Your input is lower than 1 or higher than 100');
         } else {
             console.log('clearBox');
-        };
+        }
     })
 }
 
