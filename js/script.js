723ef95fbe0feaa73639e9ea0175e95054f4a2d9
Shishi <andreiusita6@gmail.com>
2025-08-08 13:53:01 +0800

    refactor: Remove extra indents in initGrid()

--- js/script.js
+++ js/script.js
@@ -9,19 +9,19 @@ function init() {
 }
 
 function initGrid () {
-        let size = prompt('Set the grid (1-100):');
-        if (size === null) {
-            return;
-        } else if (size.length === 0) {
-            alert('Empty input');
-        } else if (!Number.isInteger(Number(size))) {
-            alert('Please enter a whole number');
-        } else if (Number(size) < 1 || Number(size) > 100) {
-            alert('Your input is lower than 1 or higher than 100');
-        } else {
-            clearBox();
-            return generateBox(size);
-        }
+    let size = prompt('Set the grid (1-100):');
+    if (size === null) {
+        return;
+    } else if (size.length === 0) {
+        alert('Empty input');
+    } else if (!Number.isInteger(Number(size))) {
+        alert('Please enter a whole number');
+    } else if (Number(size) < 1 || Number(size) > 100) {
+        alert('Your input is lower than 1 or higher than 100');
+    } else {
+        clearBox();
+        return generateBox(size);
+    }
 }
 
 function clearBox() {
