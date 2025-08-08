f94c04c34ff7ae2b1c77857d3f1ecf6de54f6498
Shishi <andreiusita6@gmail.com>
2025-08-08 08:47:33 +0800

    test: Add for loop for color generation

--- js/script.js
+++ js/script.js
@@ -48,6 +48,9 @@ function generateBox(size) {
 
 function generateColors () {
     let colors = [];
+    for (let i = 0; i < 3; i++) {
+        console.log(Math.floor(Math.random() * 256));
+    }
 }
 
 init();
